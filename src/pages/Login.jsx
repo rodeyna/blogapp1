38615588f3b41ae11b255cf1c40a0e5c1daa9b5f
marialachachi <mariamanel.lachachi@gmail.com>
2025-02-13 23:34:07 +0100

    login html and css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import '../style/form.css'
 import { Link } from 'react-router-dom'
+
 const Login = () => {
     const [User, setUser] = useState({
         username: "",
@@ -23,34 +24,51 @@ const Login = () => {
     
     return (
         <div className="form">
-        <h2 className="form-title">Login</h2>
-        <form className="login-form" onSubmit={handleSubmit}>
-          <div className="input-container">
-          
-          <input
-            type="text"
-           name='name'
-           placeholder="Enter your username"
-            value={User.username}
-            onChange={handelChange}
-          />
+          <div className='img'></div>
+          <div className='mainlog'>
+            <div className='log'>
+              <div className="intro">
+                <h2 className="form-title">Login</h2>
+                <p className='cleargrey'>Welcome back! Please login to your <br /> account.</p>
+              </div>
+              <form className="login-form" onSubmit={handleSubmit}>
+                <div className="input-container" >
+                  <label className="cleargrey">User Name</label>
+                  <input
+                  type="text"
+                  name='name'
+                  placeholder="Enter your username"
+                  value={User.username}
+                  onChange={handelChange}
+                />
+                </div>
+                <div className="input-container">
+                  <label className="cleargrey">Password</label>
+                  <input
+                    type="password"
+                    name='password'
+                    placeholder="Enter password"
+                    value={User.password}
+                    onChange={handelChange}
+                  />
+                </div>
+                <div className="param">
+                  <div className="remember">
+                   <input type="checkbox" />
+                    <p>Remember Me</p>
+                  </div>
+                  <div className="forgot">
+                    <p>Forgot Password?</p>
+                  </div>
+                </div>
+                <button className="submit" type="submit">Login</button>
+                <div class="signup-link">
+                  <p className='cleargrey'>No account?</p>
+                  <Link  to='/Signup'><p>Sign up</p></Link>
+                </div>
+              </form>
+            </div>
           </div>
-          <div className="input-container">
-         
-          <input
-            type="password"
-              name='password'
-              placeholder="Enter password"
-            value={User.password}
-            onChange={handelChange}
-          />
-          </div>
-          <button className="submit" type="submit">Sign up</button>
-          <p class="signup-link">
-          No account?
-               <Link  to='/Signup'><p>Sign up</p></Link>
-            </p>
-        </form>
         </div>
     );
 }
