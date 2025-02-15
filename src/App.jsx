4ece36cfeb90a38d718fd93cd0dfe2f345c3d115
amine <a.toubalseghir@univ-boumerdes.dz>
2025-02-15 11:24:19 +0100

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/login';
+import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Addblog from './pages/Addblog';
 import Edit from './pages/Edit';
