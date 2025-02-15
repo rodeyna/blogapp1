68a4131d4b45952a4edf650a1f6537a9c06f543b
rodeyna <ahmidrodeyna@gmail.com>
2025-02-15 11:21:26 +0100

    hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../style/hero.css"; 
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>Publish your passions, your way</h1>
+        <p>hare your thoughts, tell your story, and connect with a global audience through insightful articles, creative ideas, and meaningful discussions all on your own terms</p>
+        <button className="hero-button">Read blogs</button>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import Navbar from '../components/navbar';
-// const Home = () => {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   useEffect(() => {
-//     const loggedIn = localStorage.getItem("isLoggedIn") === "true";
-//     setIsLoggedIn(loggedIn);
-//   }, []);
-//   const [posts, setPosts] = useState([]);
-//   useEffect(() => {
-//     const savedPosts = localStorage.getItem('blogPosts');
-//     if (savedPosts) {
-//       setPosts(JSON.parse(savedPosts));
-//     }
-//   }, []);
-//   return (
-//     <>
-//       <Navbar />
-//       <h1>Home</h1>
-//       {isLoggedIn && (
-//         <Link to="/Addblog">
-//           <p>Add a new blog</p>
-//         </Link>
-//       )}   {posts.length > 0 ? (
-//         <div className="blog-posts">
-//           {posts.map((post, index) => (
-//             <div key={index} className="blog-post">
-//               <h2>{post.name}</h2>
-//               <img src={post.imgUrl} alt={post.name} style={{ width: '100%', maxWidth: '400px' }} />
-//               <p>{post.text}</p>
-//             </div>
-//           ))}
-//         </div>
-//       ) : (
-//         <p>No blog posts available. Add a new blog post!</p>
-//       )}
 
-//     </>
-//   );
-// };
-// export default Home;
 
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
